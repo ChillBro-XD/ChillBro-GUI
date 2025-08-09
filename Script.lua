a315c30135a96ec55857fb7b242c0a816ca894e5
Comando-Script <arthurmelzi.dossantos123@gmail.com>
2025-08-09 18:16:03 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,5 @@
 -- Load Kavo UI Library
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 
 -- Create Window
 local Window = Library.CreateLib("Comando's GUI", "BloodTheme")
